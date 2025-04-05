135e25959475eda177091c9e4cb076fe45f01de4
Rainbeau <63938763+Kharjai@users.noreply.github.com>
2025-04-05 00:35:13 -0600

    Add ship systems manager

---
+++ Assets/_Content/Scripts/Ship/ShipSystemsManager.cs
@@ -0,0 +1,120 @@
+using NUnit.Framework;
+using System;
+using UnityEngine;
+
+[Serializable]
+public class ShipSystemsManager : MonoBehaviour
+{
+	#region Singleton
+	private static ShipSystemsManager _instance = null;
+    /// <summary>
+    /// Singleton instance of the game manager.
+    /// </summary>
+    public static ShipSystemsManager Instance
+    { 
+        get
+        {
+            return _instance ?? (_instance = new ShipSystemsManager());
+        }
+    }
+	#endregion Singleton
+
+	#region Properties
+	#region Heat
+	/// <summary>
+	/// External Temperature (C) - About +30C/1km 
+	/// </summary>
+	public float ExternalTemperature {  get; protected set; }
+    /// <summary>
+    /// Internal Temperature (C)
+    /// </summary>
+    public float InternalTemperature { get; protected set; }
+    /// <summary>
+    /// Total heat into the ship from the environment (kWt)
+    /// </summary>
+    public float AmbientHeatInflux { get; protected set; }
+    /// <summary>
+    /// Total heat generated from ship systems (kWt)
+    /// </summary>
+    public float TotalSystemsHeat { get; protected set; }
+    /// <summary>
+    /// Total heat leaving the ship via cooling (kWt)
+    /// </summary>
+    public float TotalCoolingLoad { get; protected set; }
+	#endregion Heat
+
+	#region Power
+    /// <summary>
+    /// Total unspent fuel in reactor (MWh)
+    /// </summary>
+    public float FuelRemaining { get; protected set; }
+    /// <summary>
+    /// Total power drawn from all systems (kWe)
+    /// </summary>
+    public float TotalPowerDemand { get; protected set; }
+    /// <summary>
+    /// Total power produced by reactor(s) (kWe)
+    /// </summary>
+    public float TotalPowerProduction { get; protected set; }
+    /// <summary>
+    /// Estimated time until out of fuel (time)
+    /// </summary>
+    public TimeSpan EstimatedRuntime { get; protected set; }
+	#endregion Power
+
+	#region Environment
+    /// <summary>
+    /// Current Fathom zone
+    /// </summary>
+    public int FathomCount { get; protected set; }
+    /// <summary>
+    /// Name of the current Fathom Region
+    /// </summary>
+    public string FathomRegionName { get; protected set; }
+    /// <summary>
+    /// Current depth of the ship's nose (m)
+    /// </summary>
+    public float Depth { get; protected set; }
+	#endregion Environment
+
+	#region Telemetry
+    /// <summary>
+    /// Current maximum allowed speed of ship (m/s)
+    /// </summary>
+    public float MaximumSpeed { get; protected set; }
+    /// <summary>
+    /// Current speed of ship (m/s)
+    /// </summary>
+    public float CurrentSpeed { get; protected set; }
+    /// <summary>
+    /// Directional heading of ship (deg)
+    /// </summary>
+    public float CurrentHeading { get; protected set; }
+    /// <summary>
+    /// Target heading of ship (deg)
+    /// </summary>
+    public float TargetHeading { get; protected set; }
+    #endregion Telemetry
+    #endregion Properties
+
+    #region Fields
+    private const float MaximumInteriorTemperature = 40;
+    #endregion Fields
+
+    #region Game Object Callbacks
+    
+	#endregion Game Object Callbacks
+
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
