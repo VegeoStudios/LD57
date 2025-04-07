bb753a10ac0259bd6c11d0866529cc32a5b1ace5
VegeoStudios <vegeo.studios@gmail.com>
2025-04-07 01:52:20 -0600

    OreSpawning

--- Assets/_Content/Scripts/OreObjectPool.cs
+++ Assets/_Content/Scripts/OreObjectPool.cs
@@ -4,7 +4,9 @@ public class OreObjectPool : ObjectPool<Ore>
 {
     public static OreObjectPool Instance { get; private set; }
 
-    public Sprite[] OreSprites;
+    public Sprite[] MetalSprites;
+    public Sprite[] CrystalSprites;
+    public Sprite[] LiquidSprites;
     public float[] ColliderRadii;
 
     private void Awake()
@@ -29,10 +31,29 @@ public class OreObjectPool : ObjectPool<Ore>
         obj.transform.position = position;
         obj.transform.rotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
 
+        Sprite[] sprites;
+        switch (item.OreType)
+        {
+            case OreType.Crystal:
+                sprites = CrystalSprites;
+                break;
+            case OreType.Metallic:
+                sprites = MetalSprites;
+                break;
+            case OreType.Liquid:
+                sprites = LiquidSprites;
+                break;
+            default:
+                sprites = MetalSprites;
+                return null;
+        }
+
         obj.Value = value;
         obj.Item = item;
-        int tierIndex = Mathf.Clamp(Mathf.FloorToInt(Mathf.Lerp(0, OreSprites.Length, value / 6)), 0, OreSprites.Length - 1);
-        obj.GetComponent<SpriteRenderer>().sprite = OreSprites[tierIndex];
+        int tierIndex = Mathf.Clamp(Mathf.FloorToInt(Mathf.Lerp(0, ColliderRadii.Length, value / 6)), 0, ColliderRadii.Length - 1);
+        SpriteRenderer spriteRenderer = obj.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprites[tierIndex];
+        spriteRenderer.color = item.Color;
         obj.GetComponent<CircleCollider2D>().radius = ColliderRadii[tierIndex];
         obj.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
         return obj;
